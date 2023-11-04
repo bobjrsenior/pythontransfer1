85b059b691c525b5ed43e1f013151aa1aa1e4831
bobjrsenior <git@bobjrsenior.pw>
2023-11-04 17:34:57 -0400

    example audio

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 from gpiozero import LED, Button
 from time import sleep
+from playsound import playsound
 
 led = LED(2)
 button = Button(14)
 
 button.when_pressed = led.on
 button.when_released = led.off
+
+playsound('audio/woahh.ogg')
 
 sleep(60)
